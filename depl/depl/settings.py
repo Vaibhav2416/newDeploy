cab44e4fd4cf4c7fac9059a1c9afcb1f3ea66d3e
Vaibhav2416 <vaibhavsultane1997@gmail.com>
2025-04-26 09:53:23 +0530

    new upd

--- depl/depl/settings.py
+++ depl/depl/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3n_fciyno@s%iaosv1mw0(824(jv5nyg16)=mt&@4#v8tx0$o-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["vaibhav1997.pythonanywhere.com"]
 
 
 # Application definition
